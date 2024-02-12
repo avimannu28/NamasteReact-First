648003c7a843dcea98908435b24f77e6ce3b29c0
avinash @ <avimannu28@gmai.com>
2024-02-13 00:48:04 +0530

    my first regular push

---
+++ app.js
@@ -0,0 +1,6 @@
+
+    const heading =React.createElement("h1",{},"This is rendered from React");
+    // console.log(heading)
+    const root=ReactDOM.createRoot(document.getElementById("root"));
+    console.log(heading)
+    root.render(heading)
